8e4399d1e2f59db26068aecb1ee82a8d20e63198
Kupulko <kirillstecko37@gmail.com>
2025-08-27 01:15:03 +0300

    footer+blue shadows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import Image from 'next/image';
 export default function HomePage() {
   return (
     <main>
+      <div className="absolute w-[1597.68px] h-[4737.14px] -left-[47.26px] top-[825.39px] bg-[rgba(0,51,255,0.07)] blur-[102px]"></div>
       <MainPage />
       <AboutUs />
       <WhatWeOffer />
