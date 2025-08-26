7f0f1b7fd2fee7283ad1eb805fad4af9d15f9f50
Kupulko <kirillstecko37@gmail.com>
2025-08-26 21:20:54 +0300

    first page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 import Link from "next/link";
 
+
 const Header = () => {
   return (
     <header className="sticky top-0 w-full h-[70px] bg-gradient-to-tr from-[#0D0D0D] to-[#4635A9] z-50">
-      <nav className="flex items-center pl-40 space-x-8 font-montserratAlt list-none h-full">
+      <nav className="flex items-center pl-40 space-x-20 font-montserratAlt list-none h-full">
         {/* Логотип */}
         <li>
           <img src="/logo.svg" alt="Logo" className="w-8 h-8" />
